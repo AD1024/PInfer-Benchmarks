b851d3a55059e34316a514e2e963452b45a80ad3
AD1024 <dh63@cs.washington.edu>
2026-01-27 18:10:37 -0500

    save scripts

--- step_1.sh
+++ step_1.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 python3 cleanup.py
 python3 run_pinfer_sequential.py
-cd rq2  
-eval $(opam env) && python3 run_pinfer_examples.py
--- step_1_draw_tables.sh
+++ step_1_draw_tables.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 mkdir -p tables
+echo "==========Table 4=========="
 python3 draw_tables.py --tables 4
+echo "==========================="
+echo "==========Table 5=========="
 python3 draw_tables.py --tables 5 --no-rerun
+echo "==========================="
+echo "==========Table 6=========="
 python3 draw_tables.py --tables 6 --no-rerun
+echo "==========================="
+
+cd rq2  
+eval $(opam env) && python3 run_pinfer_examples.py
+echo "==========Table 6 -- Verification Time=========="
+cat pv_times.txt
+echo "==============================================="
+cd ..
+
 mv table_4.txt table_5.txt table_6.txt tables/
 mv rq2/pv_times.txt tables/table_6_verifier_time.txt
